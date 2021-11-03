5ee6bfeaea02169893a76d080dd57e4f5862dd96
yaochuang <ych_ding@163.com>
2021-11-03 12:26:33 +0800

    clean up

--- tutorial-solid-wireframe/src/viewer.cpp
+++ tutorial-solid-wireframe/src/viewer.cpp
@@ -37,13 +37,6 @@ std::unique_ptr<Camera> buildCamera(const MeshType & mesh)
 
 void Viewer::Run()
 {
-    m_quad = std::make_unique<Quad>();
-    m_cube = std::make_unique<Cube>();
-
-    m_cube->SetMaterial(&m_brickWall);
-    m_quad->SetMaterial(&m_brickWall);
-    m_active_simpleMesh = m_quad.get();
-
     //< second stage init
     MeshBin meshes{ m_objPath };
 
@@ -53,16 +46,11 @@ void Viewer::Run()
 
     if (m_simple_mesh_mode)
     {
-        m_camera = buildCamera(*m_active_simpleMesh);
     }
     else
     {
         m_camera = buildCamera(meshes);
     }
-
-    //< it should lighting here
-    m_pointLight = std::make_unique<PointLight>(
-        glm::vec3{0.0, 0.1f , 0.3 * m_active_simpleMesh->LogestDim()}, glm::vec3{3.0f});
 
     glfwCallbackData cb{ m_camera.get(), m_animation_mode, GetRenderSetting(), GetDisplayOption()};
     glfwSetWindowUserPointer(m_window, &cb);
@@ -73,7 +61,7 @@ void Viewer::Run()
         evaluateDirtyValue();
         animateExplode();
         animateCamera(*m_camera);
-        render(meshes, *m_active_simpleMesh, *m_camera);
+        render(meshes, SimpleMesh{}, *m_camera);
     } while(glfwGetKey(m_window, GLFW_KEY_ESCAPE) != GLFW_PRESS && glfwWindowShouldClose(m_window) == 0);
 
     GUI::CleanUp();
@@ -81,18 +69,7 @@ void Viewer::Run()
 
 void Viewer::evaluateDirtyValue()
 {
-    if (m_picked_mesh_name == "Quad")
-    {
-        m_active_simpleMesh = m_quad.get();
-    }
-    else if (m_picked_mesh_name == "Cube")
-    {
-        m_active_simpleMesh = m_cube.get();
-    }
-    else
-    {
 
-    }
 }
 
 void Viewer::animateExplode()
@@ -250,36 +227,6 @@ void Viewer::renderLight(Light& light, const Camera& camera)
 
 void Viewer::renderSimpleMesh(SimpleMesh& simplemesh, const Camera& camera)
 {
-    Material *mat = simplemesh.GetMaterial();
-    Shader* shader = mat->GetShader();
-    shader->Active();
-    shader->SetMat4("M", simplemesh.GetWorldMatrix());
-    shader->SetMat4("V", camera.viewMatrix());
-    shader->SetMat4("P", camera.projMatrix());
-    shader->SetVec3("u_viewer_pos", camera.eye());
-
-    shader->SetVec3("u_light_pos",  m_pointLight->GetPosition());
-    shader->SetVec3("u_light_color",m_pointLight->GetColor());
-
-    shader->SetFloat("u_clip_plane_dist", m_clip_plane_distance);
-
-    shader->SetInt("diffuseMap", 0);
-    shader->SetInt("normalMap", 1);
-    if (m_enable_normal_map) //< control from view
-    shader->SetInt("use_normal_map", 1);
-    else
-    shader->SetInt("use_normal_map", 0);
-    mat->ActiveDiffuseMap();
-    mat->ActiveNormalMap();
-    glBindVertexArray( simplemesh.vao() );
-    glDrawArrays( GL_TRIANGLES, 0, simplemesh.vertex_num() );
-    GLenum errorCheckValue = glGetError();
-    if (errorCheckValue != GL_NO_ERROR)
-    {
-        //Err("Draw: {}", gluErrorString(errorCheckValue));
-    }
-
-    renderLight(*m_pointLight, camera);
 }
 
 void Viewer::initOpenGLShaders()
@@ -294,21 +241,12 @@ void Viewer::initOpenGLShaders()
 
 void Viewer::initMaterials(void)
 {
-    //< shader cache someday
-    m_brickWall.Init("shaders/brickWall.vs.glsl", "shaders/brickWall.fs.glsl");
-    m_light_material.Init("shaders/light.vs.glsl", "shaders/light.fs.glsl");
-
     GetTextureCache().Add("textures/brickwall_diffuse.jpg");
     GetTextureCache().Add("textures/brickwall_normal.jpg");
 
     GetTextureCache().Add("textures/bricks2_diffuse.jpg");
     GetTextureCache().Add("textures/bricks2_normal.jpg");
     GetTextureCache().Add("textures/bricks2_disp.jpg");
-
-    m_brickWall.m_diffuseTexName = "bricks2_diffuse.jpg";
-    m_brickWall.m_normalTexName = "bricks2_normal.jpg";
-    m_brickWall.m_dirty = true;
-    m_brickWall.m_reevaluate = true;
 }
 
 int Viewer::initWindow()
@@ -464,7 +402,8 @@ static void drawUI(Viewer &viewer)
                 }
             }
             ImGui::SliderFloat("Clip plane distance", &viewer.m_clip_plane_distance, -0.5f, 0.5f);
-
+            ImGui::Separator();
+            ImGui::Checkbox("Double side lighting", &viewer.m_double_side_lighting);
             ImGui::EndMenu();
         }
         if (ImGui::BeginMenu(ICON_FA_EYE " View"))
@@ -478,8 +417,6 @@ static void drawUI(Viewer &viewer)
         if (ImGui::BeginMenu(ICON_FA_WINDOWS " Settings"))
         {
             ImGui::Checkbox("VSync", &viewer.m_enable_vsync);
-            ImGui::Separator();
-            ImGui::Checkbox("Double side lighting", &viewer.m_double_side_lighting);
             ImGui::Separator();
             ImGui::ColorEdit3("Mesh Color", &viewer.m_mesh_color.x);
             ImGui::Separator();
@@ -528,9 +465,6 @@ static void drawHelp(RenderSetting &setting)
     {
         ImGui::Text("Help    : ");
         ImGui::Text("Key stroke  : ");
-        ImGui::Text("         'T' : toggle tesselation ");
-        ImGui::Text("         ']' : increase tessellate level when enable tesselation ");
-        ImGui::Text("         '[' : decrease tessellate level when enable tesselation ");
         ImGui::Text("         ' ' : loop animation mode ");
         ImGui::End();
     }
