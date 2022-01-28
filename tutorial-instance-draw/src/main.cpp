e18697b8b6f158a0d81cae0c96991bb45c369feb
yaochuang <ych_ding@163.com>
2022-01-28 22:20:10 +0800

    temp

--- tutorial-instance-draw/src/main.cpp
+++ tutorial-instance-draw/src/main.cpp
@@ -32,7 +32,7 @@ int main(int argc, char **argv)
         modelPath = "Model/bunny.obj";
     }
 
-    Viewer viewer{"Viewer", 1280, 720};
+    Viewer viewer{"Instance Draw Viewer", 1280, 720};
     viewer.SetModel(modelPath);
     viewer.Run();
 
