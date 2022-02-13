c1a186dcf59ef449f68c87b109d619388bc2c918
yaochuang <ych_ding@163.com>
2022-02-13 16:55:38 +0800

    add new graphics utility.

--- common/graphicsUtility.h
+++ common/graphicsUtility.h
@@ -54,3 +54,127 @@ inline static void SyncGPU()
         wait_return = glClientWaitSync(sync_fence, GL_SYNC_FLUSH_COMMANDS_BIT, 1);
     glDeleteSync(sync_fence);
 }
+
+class Buffer
+{
+private:
+	GLuint m_id{ kInvalidGLId };
+
+public:
+	Buffer() = default;
+
+	Buffer(const Buffer&) = delete;
+
+	Buffer(Buffer&& buffer) noexcept
+		: m_id(buffer.m_id)
+	{
+		buffer.m_id = kInvalidGLId;
+	}
+
+	Buffer& operator= (const Buffer&) = delete;
+
+	/*Buffer& operator= (Buffer&& buffer) noexcept
+	{
+		m_id = buffer.m_id; buffer.m_id = kInvalidOglId;
+		return *this;
+	}*/
+
+	~Buffer()
+	{
+		if (IsValidGLId(m_id))
+			glDeleteBuffers(1, &m_id);
+	}
+
+	void Initialize()
+	{
+		glCreateBuffers(1, &m_id);
+	}
+
+	GLuint Get() const { return m_id; }
+
+	void Storage(GLsizei bytes, GLbitfield flags)
+	{
+		// https://www.khronos.org/registry/OpenGL-Refpages/gl4/html/glBufferStorage.xhtml
+		// since GL 4.5
+		glNamedBufferStorage(m_id, bytes, nullptr, flags);
+	}
+
+	template<class T>
+	void Storage(T* begin, T* end, GLbitfield flags)
+	{
+		// https://www.khronos.org/registry/OpenGL-Refpages/gl4/html/glBufferStorage.xhtml
+		// since GL 4.5
+		glNamedBufferStorage(m_id, (end - begin) * sizeof(T), begin, flags);
+	}
+
+	//never use Data() with Storage()
+	template<class T>
+	void Data(T* begin, T* end, GLenum usage)
+	{
+		glNamedBufferData(m_id, (end - begin) * sizeof(T), begin, usage);
+	}
+
+	template<class T>
+	void SubData(GLsizei byte_offset, const T* begin, const T* end)
+	{
+		glNamedBufferSubData(m_id, byte_offset, (end - begin) * sizeof(T), begin);
+	}
+
+	GLint GetByteCount() const
+	{
+		GLint c = 0;
+		glGetNamedBufferParameteriv(m_id, GL_BUFFER_SIZE, &c);
+		return c;
+	}
+
+	void Bind(GLenum target) const { glBindBuffer(target, m_id); }
+
+	void BindBase(GLenum target, GLuint index) const
+	{
+		glBindBufferBase(target, index, m_id);
+	}
+};
+
+class AtomicCounter
+{
+private:
+	Buffer m_buffer;
+	GLuint* m_mapped{ nullptr };
+
+public:
+	~AtomicCounter()
+	{
+		if (m_mapped) glUnmapNamedBuffer(m_buffer.Get());
+	}
+
+	void Initialize()
+	{
+		m_buffer.Initialize();
+		GLbitfield flags = GL_MAP_READ_BIT | GL_MAP_WRITE_BIT | GL_MAP_PERSISTENT_BIT | GL_MAP_COHERENT_BIT;
+		m_buffer.Storage(sizeof(GLuint), flags);
+
+		// https://www.khronos.org/registry/OpenGL-Refpages/gl4/html/glMapBufferRange.xhtml
+		// since GL4.5
+		m_mapped = (GLuint*)glMapNamedBufferRange(m_buffer.Get(), 0, sizeof(GLuint), flags);
+		*m_mapped = 0u; // assign an initial value 0
+	}
+
+	void BindAsAtomicCounter(GLuint index) const { glBindBufferBase(GL_ATOMIC_COUNTER_BUFFER, index, m_buffer.Get()); }
+	void BindAsShaderStorage(GLuint index) const { glBindBufferBase(GL_SHADER_STORAGE_BUFFER, index, m_buffer.Get()); }
+
+	      GLuint& GetValue()       { return *m_mapped; }
+	const GLuint& GetValue() const { return *m_mapped; }
+
+	GLuint& SyncAndGetValue()
+	{
+		SyncGPU();
+		return *m_mapped;
+	}
+	const GLuint& SyncAndGetValue() const
+	{
+		SyncGPU();
+		return *m_mapped;
+	}
+
+	void Reset() { SyncGPU(); *m_mapped = 0; }
+};
