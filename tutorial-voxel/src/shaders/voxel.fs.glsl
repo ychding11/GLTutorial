6f4181c2e59f584f636baafe4fc4d03fbf1a7658
yaochuang <ych_ding@163.com>
2022-02-24 22:46:40 +0800

    temp

--- tutorial-voxel/src/shaders/voxel.fs.glsl
+++ tutorial-voxel/src/shaders/voxel.fs.glsl
@@ -19,7 +19,8 @@ void main()
      uint curIndex = atomicCounterIncrement(uCounter);
      if (u_count_voxel_only != 0)
      {
-        //It cause crash in intel gpu
+        //< It cause crash in intel gpu
+        //< I have tested, it works in nvidia video card
      	uvec3 uvoxel_pos = clamp(uvec3(gVoxelPos), uvec3(0u), uvec3(1024 - 1u));
 		uFragmentList[curIndex].x = uvoxel_pos.x | (uvoxel_pos.y << 12u) | ((uvoxel_pos.z & 0xffu) << 24u); //only have the last 8 bits of uvoxel_pos.z
 		uFragmentList[curIndex].y = ((uvoxel_pos.z >> 8u) << 28u) | 0 | (0 << 8u) | (10 << 16u);
